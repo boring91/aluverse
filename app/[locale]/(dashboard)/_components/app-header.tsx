4743cda0cc620c7e1119c624e5ffd9d923925bf5
boring <m2008m1033m@gmail.com>
2025-12-26 08:42:13 +0400

    refactor(header-sidebar): streamline user navigation by integrating user menu into sidebar
    Refactor the AppHeader and AppSidebar components to enhance user navigation. The UserNav component has been moved from the header to the sidebar, providing a more cohesive user experience. This change simplifies the header by removing unnecessary elements and consolidates user-related actions within the sidebar, improving overall maintainability and usability.

--- app/[locale]/(dashboard)/_components/app-header.tsx
+++ app/[locale]/(dashboard)/_components/app-header.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useIsMounted } from "@/hooks/use-is-mounted";
-import { useRouter } from "@/i18n/navigation";
-import { signOut, useSession } from "@/lib/auth-client";
-import { useConfirm } from "@/lib/confirm-context";
-import { LogOutIcon, MoonIcon, SunIcon, UserIcon } from "lucide-react";
-import { useTranslations } from "next-intl";
+import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 const ThemeSwitcher = () => {
@@ -31,52 +21,6 @@ const ThemeSwitcher = () => {
     );
 };
 
-const UserNav = () => {
-    const { confirm } = useConfirm();
-
-    const { data } = useSession();
-    const tc = useTranslations("Common");
-    const router = useRouter();
-
-    const handleSignOut = async () => {
-        await signOut();
-        router.push("/login");
-    };
-
-    return (
-        <>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                        <UserIcon />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuItem className="flex flex-col gap-0.5 items-start">
-                        <p className="text-sm">{data?.user?.name}</p>
-                        <p className="text-muted-foreground text-xs">
-                            {data?.user?.email}
-                        </p>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                        variant="destructive"
-                        onClick={() =>
-                            confirm({
-                                title: tc("logout"),
-                                description: tc("areYouSureYouWantToLogout"),
-                                onConfirm: handleSignOut,
-                            })
-                        }
-                    >
-                        <LogOutIcon />
-                        <span>{tc("logout")}</span>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </>
-    );
-};
-
 export const AppHeader = () => {
     return (
         <header className="h-16 flex items-center px-4 border-b w-full justify-between">
@@ -84,7 +28,6 @@ export const AppHeader = () => {
 
             <div className="flex items-center gap-2">
                 <ThemeSwitcher />
-                <UserNav />
             </div>
         </header>
     );
--- app/[locale]/(dashboard)/_components/app-sidebar.tsx
+++ app/[locale]/(dashboard)/_components/app-sidebar.tsx
@@ -1,17 +1,37 @@
 "use client";
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
     Sidebar,
     SidebarContent,
+    SidebarFooter,
     SidebarGroup,
     SidebarGroupContent,
     SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    useSidebar,
 } from "@/components/ui/sidebar";
-import { Link, usePathname } from "@/i18n/navigation";
-import { ChartPieIcon, CoinsIcon, FolderIcon, HomeIcon } from "lucide-react";
+import { Link, usePathname, useRouter } from "@/i18n/navigation";
+import { signOut, useSession } from "@/lib/auth-client";
+import { useConfirm } from "@/lib/confirm-context";
+import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+import {
+    ChartPieIcon,
+    ChevronsUpDown,
+    CoinsIcon,
+    FolderIcon,
+    HomeIcon,
+    LogOutIcon,
+} from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const items = [
@@ -40,6 +60,91 @@ const items = [
     },
 ] as const;
 
+const UserNav = () => {
+    const { confirm } = useConfirm();
+
+    const { data } = useSession();
+    const { isMobile } = useSidebar();
+
+    const tc = useTranslations("Common");
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+        await signOut();
+        router.push("/login");
+    };
+
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton
+                        size="lg"
+                        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                    >
+                        <Avatar className="h-8 w-8 rounded-lg grayscale">
+                            {/* <AvatarImage
+                                src={user.avatar}
+                                alt={data?.user.name}
+                            /> */}
+                            <AvatarFallback className="rounded-lg">
+                                {data?.user.name.substring(0, 2)}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                            <span className="truncate font-medium">
+                                {data?.user.name}
+                            </span>
+                            <span className="text-muted-foreground truncate text-xs">
+                                {data?.user.email}
+                            </span>
+                        </div>
+                        <ChevronsUpDown className="ml-auto size-4" />
+                    </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                    className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                    side={isMobile ? "bottom" : "right"}
+                    align="end"
+                    sideOffset={4}
+                >
+                    <DropdownMenuLabel className="p-0 font-normal">
+                        <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarFallback className="rounded-lg">
+                                    {data?.user.name.substring(0, 2)}
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-medium">
+                                    {data?.user.name}
+                                </span>
+                                <span className="text-muted-foreground truncate text-xs">
+                                    {data?.user.email}
+                                </span>
+                            </div>
+                        </div>
+                    </DropdownMenuLabel>
+
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                        onClick={() =>
+                            confirm({
+                                title: tc("logout"),
+                                description: tc("areYouSureYouWantToLogout"),
+                                onConfirm: handleSignOut,
+                            })
+                        }
+                    >
+                        <LogOutIcon />
+                        {tc("logout")}
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+};
+
 export const AppSidebar = () => {
     const t = useTranslations("Common");
     const pathname = usePathname();
@@ -50,6 +155,8 @@ export const AppSidebar = () => {
         }
         return link !== "/" && pathname.startsWith(link);
     };
+
+    const { data } = useSession();
 
     return (
         <Sidebar>
@@ -81,6 +188,15 @@ export const AppSidebar = () => {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
+            <SidebarFooter>
+                <SidebarGroup>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <UserNav />
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarFooter>
         </Sidebar>
     );
 };
